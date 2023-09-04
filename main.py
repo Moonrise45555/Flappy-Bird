ec36d46f6569a843389765274431e8fc3c820022
gcanalfe <gcanalfe@students.uni-mainz.de>
2023-09-04 13:55:33 +0200

    finished pipe spawning

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import random
 import sys
 import pygame as pg
-import pipe_generation as pipe
+import pipe as pipe
 import game_engine
 from pygame.locals import *
 from time import sleep
 window_width = 600
 window_height = 499
-
+pipes = []
 window = pg.display.set_mode((window_width, window_height))
-
+FRAMELENGTH = 1/20
 pg.init()
+timer = 0
+Spawndelay = 30
 while True:
-    sleep(0.5)
+    sleep(FRAMELENGTH)
+    
+    timer += 1
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
     window.fill("white")
+    if timer == Spawndelay:
+        pipes.append(pipe.generate_pipe())
+        timer = 0
     pg.draw.circle(window,"black",pg.Vector2(10,10),10)
-    game_engine.draw_pipe(pipe.generate_pipe(),window)
+    pipe.draw_and_move_pipes(pipes,window)
     pg.display.flip()
 
 
--- pipe.py
+++ pipe.py
@@ -4,10 +4,21 @@ import pygame as pg
 def generate_pipe():
     color = "black"
     hole_height = 50
-    pos = pg.Vector2(400,0)
+    pos = pg.Vector2(550,0)
     top_pipe_height = random.randint(25,499-hole_height -25)
     bottom_pipe_height = 499 - top_pipe_height - hole_height
     pipe_width = 50
     bottom_rectangle = pg.Rect(pg.Vector2(pos.x + 0,0),pg.Vector2(pipe_width,bottom_pipe_height))
     top_rectangle = pg.Rect(pg.Vector2(pos.x + 0,bottom_pipe_height + hole_height),pg.Vector2( pipe_width,top_pipe_height))
+    
     return [top_rectangle,bottom_rectangle]
+    
+
+def draw_and_move_pipes(pipes,screen):
+    for i in pipes:
+        for pipe in i:
+            pg.draw.rect(screen,"black",pipe)
+            pipe.topleft = (pipe.topleft[0] - 5,pipe.topleft[1])
+            pipe.bottomright = (pipe.bottomright[0] - 5,pipe.bottomright[1])
+
+            
